a7f5b584c1cbe2b1e5594fded0acde7f78551c4b
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-11-09 14:32:39 -0300

    Creating forAdmin and forUser endpoint

--- backend/src/main/java/com/david/backend/controller/UserController.java
+++ backend/src/main/java/com/david/backend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.david.backend.controller;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +25,15 @@ public class UserController {
     @PostMapping({ "/registerNewUser" })
     public User registerNewUser(@RequestBody User user) {
         return userService.registerNewUser(user);
+    }
+
+    @GetMapping({ "/forAdmin" })
+    public String forAdmin() {
+        return "This URL is only accessible to the admin";
+    }
+
+    @GetMapping({ "/forUser" })
+    public String forUser() {
+        return "This URL is only accessible to the user";
     }
 }
