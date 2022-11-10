1ed7a8d24da84347ae53cca9c3dd6f37182c9e37
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-11-10 12:43:32 -0300

    Testing the application

--- backend/src/main/java/com/david/backend/controller/UserController.java
+++ backend/src/main/java/com/david/backend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.david.backend.controller;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,11 +29,13 @@ public class UserController {
     }
 
     @GetMapping({ "/forAdmin" })
+    @PreAuthorize("hasRole('Admin')")
     public String forAdmin() {
         return "This URL is only accessible to the admin";
     }
 
     @GetMapping({ "/forUser" })
+    @PreAuthorize("hasRole('User')")
     public String forUser() {
         return "This URL is only accessible to the user";
     }
--- backend/src/main/java/com/david/backend/service/UserService.java
+++ backend/src/main/java/com/david/backend/service/UserService.java
@@ -25,6 +25,14 @@ public class UserService {
     private PasswordEncoder passwordEncoder;
 
     public User registerNewUser(User user) {
+
+        Role role = roleDao.findById("User").get();
+
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        user.setRole(roles);
+
+        user.setUserPassword(getEncodedPassword(user.getUserPassword()));
         return userDao.save(user);
     }
 
@@ -42,22 +50,12 @@ public class UserService {
         User adminUser = new User();
         adminUser.setUserFirstName("admin");
         adminUser.setUserLastName("admin");
-        adminUser.setUserName("admin123");
+        adminUser.setUserName("admin");
         adminUser.setUserPassword(getEncodedPassword("admin@pass"));
         Set<Role> adminRoles = new HashSet<>();
         adminRoles.add(adminRole);
         adminUser.setRole(adminRoles);
         userDao.save(adminUser);
-
-        User user = new User();
-        user.setUserFirstName("David");
-        user.setUserLastName("Mateus");
-        user.setUserName("davidmateus");
-        user.setUserPassword(getEncodedPassword("123456"));
-        Set<Role> userRoles = new HashSet<>();
-        userRoles.add(userRole);
-        user.setRole(userRoles);
-        userDao.save(user);
     }
 
     public String getEncodedPassword(String password) {
