09f8910dbd53b0fd7d8143909d67899faea00190
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-11-14 10:29:17 -0300

    Creating API to Get All Product Details

--- backend/src/main/java/com/david/backend/controller/ProductController.java
+++ backend/src/main/java/com/david/backend/controller/ProductController.java
@@ -2,11 +2,13 @@ package com.david.backend.controller;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +51,10 @@ public class ProductController {
         }
 
         return imageModels;
+    }
+
+    @GetMapping({ "/getAllProducts" })
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
